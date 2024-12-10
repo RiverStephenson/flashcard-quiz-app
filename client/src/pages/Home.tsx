bf8cd7d26e11b390213d7232aa7d0c94dd57cbe5
RiverStephenson <riverstephenson06@outlook.com>
2024-12-10 10:15:32 -0600

    adds home page functionality

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-
+import { Link } from "react-router-dom";
 
 const Home = () => {
   
@@ -7,8 +7,8 @@ const Home = () => {
     return(
         <main>
             <h1>Welcome to QwizMe</h1>
-        <button type='button'>Sign Up</button>
-        <button type='button'>Log In</button>
+        <Link to='/signup'>Sign Up</Link>
+        <Link to='/login'>Log In</Link>
         </main>
     );
 };
