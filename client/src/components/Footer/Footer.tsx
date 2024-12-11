1f74a9c785401aa267873ed25ddbdba05798e2cb
Michael Fleming <mikahl171@gmail.com>
2024-12-10 18:45:10 -0600

    added River's footer code back in

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import './footer.css'
 
 const Footer: React.FC = () => {
 
@@ -6,8 +7,8 @@ const Footer: React.FC = () => {
 
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container text-center mb-5">
-        <h4>
+      <div className="container-A">
+        <div className='otherDiv'>
           Made with{' '}
           <span
             className="emoji"
@@ -18,7 +19,11 @@ const Footer: React.FC = () => {
             ❤️
           </span>{' '}
           by the QwizMe team.
-        </h4>
+        </div>
+        <div className='links-a'>
+        <nav><a className='githubLinks' href='https://github.com/RiverStephenson'><img src='https://logos-download.com/wp-content/uploads/2016/09/GitHub_logo.png'></img>River Stephenson</a></nav>
+        <nav><a className='githubLinks' href='https://github.com/Bluzke'><img src='https://logos-download.com/wp-content/uploads/2016/09/GitHub_logo.png'></img>Justin Silber</a></nav>
+        </div>
       </div>
     </footer>
   );
